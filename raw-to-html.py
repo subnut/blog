a8773d87dcd8c3b0df16a85f724684767483fa86
Subhaditya Nath <sn03.general@gmail.com>
2021-03-26 12:23:59 +0530

    Major improvements

--- raw-to-html.py
+++ raw-to-html.py
@@ -78,22 +78,27 @@ def htmlize(lines) -> str:
     print = lambda x: OUTPUT.append(x)
 
     TABLE_MODE = False
-    CODE_OPEN = False
-    BOLD_OPEN = False
-    ITALIC_OPEN = False
     CODEBLOCK_OPEN = False
     HTML_TAG_OPEN = False
+
+    char_DICT = {
+        "*": {"open": False, "tag": "b"},
+        "_": {"open": False, "tag": "i"},
+        "`": {"open": False, "tag": "code"},
+    }
 
     for linenr, line in enumerate(lines):
 
         # Table
         if line.startswith("<table") and not TABLE_MODE:
             TABLE_MODE = True
+            print(line)
             continue
 
         # Table end
         if line == "</table>\n" and TABLE_MODE:
             TABLE_MODE = False
+            print(line)
             continue
 
         # Blank lines
@@ -107,22 +112,27 @@ def htmlize(lines) -> str:
         if line == "```\n":
             if CODEBLOCK_OPEN:
                 CODEBLOCK_OPEN = False
-                print("</code></pre>")
+                print("</code></pre>\n")
             else:
                 CODEBLOCK_OPEN = True
-                print("<pre><code>")
+                print("<pre><code>\n")
             continue
 
         # Headings using '#'
+        H_LEVEL = 0
         if line[0] == "#":
             H_LEVEL = 2
             if line[1] == "#":
                 H_LEVEL = 3
                 if line[2] == "#":
                     H_LEVEL = 4
-                    if line[3] == "#":
-                        H_LEVEL = 5
-            line = f"<h{H_LEVEL}>{line[H_LEVEL-1:].strip()}</h{H_LEVEL}>\n"
+            print(f"<h{H_LEVEL}>")
+            line = line[H_LEVEL - 1 :].strip()
+            # line = f"<h{H_LEVEL}>{line[H_LEVEL-1:].strip()}</h{H_LEVEL}>\n"
+
+        # Support for Tables
+        if TABLE_MODE:
+            print("<tr><td>")
 
         for index, char in enumerate(line):
             if not CODEBLOCK_OPEN:
@@ -131,10 +141,13 @@ def htmlize(lines) -> str:
                     char == "<"
                     and not HTML_TAG_OPEN
                     and (line[index + 1] == "/" or line[index + 1].isalpha())
-                    and line[index - 1] != "\\"
                 ):
-                    HTML_TAG_OPEN = True
-                    print(char)
+                    if line[index - 1] != "\\":
+                        HTML_TAG_OPEN = True
+                        print(char)
+                    else:
+                        OUTPUT.pop()  # remove the '\' that escaped char
+                        print(char)
                     continue
 
                 # Whether the '>' is part of an HTML tag
@@ -144,49 +157,53 @@ def htmlize(lines) -> str:
                     continue
 
                 if not HTML_TAG_OPEN:
+
                     # Linebreak if two spaces at line end
                     if char == "\n" and index > 1 and line[-3:-1] == "  ":
                         print("<br>\n")
                         continue
 
-                    # Whether the '*' is for bold
-                    if char == "*" and line[index - 1] != "\\":
-                        if BOLD_OPEN:
-                            BOLD_OPEN = False
-                            print("</b>")
+                    if char in "*_`":
+                        if line[index - 1] == "\\":
+                            OUTPUT.pop()
+                            print(char)
+                            continue
+                        char_dict = char_DICT[char]
+                        if char_dict["open"]:
+                            if line[index + 1].isspace():
+                                char_dict["open"] = False
+                                print(f"</{char_dict['tag']}>")
+                                continue
                         else:
-                            BOLD_OPEN = True
-                            print("<b>")
-                        continue
-
-                    # Whether the '_' is for italic
-                    if char == "_" and line[index - 1] != "\\":
-                        if ITALIC_OPEN:
-                            ITALIC_OPEN = False
-                            print("</i>")
-                        else:
-                            ITALIC_OPEN = True
-                            print("<i>")
-                        continue
-
-                    # Whether the '`' is for code
-                    if char == "`" and line[index - 1] != "\\":
-                        if CODE_OPEN:
-                            CODE_OPEN = False
-                            print("</code>")
-                        else:
-                            CODE_OPEN = True
-                            print("<code>")
+                            if index == 0 or line[index - 1].isspace():
+                                char_dict["open"] = True
+                                print(f"<{char_dict['tag']}>")
+                                continue
+                        print(char)
                         continue
 
                     # Support for tables
                     if TABLE_MODE:
-                        if (
-                            char == "|"
-                            and not (CODE_OPEN or BOLD_OPEN or ITALIC_OPEN)
-                            and line[index - 1] != "\\"
-                        ):
+
+                        char_DICT_any_char_open = False
+                        for char_dict in char_DICT.values():
+                            char_DICT_any_char_open = (
+                                char_DICT_any_char_open or char_dict["open"]
+                            )
+                            # i.e. if any char of char_DICT is "open",
+                            # char_DICT_any_char_open shall become True
+
+                        if char == "|" and not char_DICT_any_char_open:
+                            if line[index - 1] == "\\":
+                                OUTPUT.pop()
+                                print(char)
+                                continue
                             print("</td><td>")
+                            continue
+
+                        if char == "\n":
+                            print("</td></tr>\n")
+                            continue
 
                 # We're inside an HTML_TAG. Don't escape.
                 else:
@@ -195,6 +212,10 @@ def htmlize(lines) -> str:
 
             # It's a normal character. Escape it.
             print(html.escape(char))
+
+        # If this line was a Heading
+        if H_LEVEL:
+            print(f"</h{H_LEVEL}>\n")
 
     return "".join(OUTPUT)
 
