cfdf88bad08f34ee6b99bb689199df7fcb004e6c
Subhaditya Nath <sn03.general@gmail.com>
2021-03-26 14:05:35 +0530

    Added publish-to-github-pages.sh

---
+++ publish-to-github-pages.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ `git status --porcelain | wc -l` != 0 ]; then
+	echo 'dirty working tree' >&2
+	echo 'please stash/commit your changes first' >&2
+	exit 1
+fi
+run() { echo "> \"$@\""; "$@"; }
+run git branch -D gh-pages
+run git checkout -b gh-pages
+run mkdir docs
+run python raw-to-html.py
+run python index-page-creator.py
+run git add .
+run git commit -m PUBLISH
+run trap : INT
+run git push --set-upstream $(git remote) gh-pages --force
+run git switch -
