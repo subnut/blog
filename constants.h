857499f966ef6dea4e3d91d00439942a2ee62322
Subhaditya Nath <sn03.general@gmail.com>
2021-04-13 10:25:00 +0530

    Added inline favicon

--- constants.h
+++ constants.h
@@ -1,9 +1,20 @@
-#define FAVICON    "<link href=data:, rel=icon>"
 #define SOURCE_EXT ".blog"
 #define SOURCE_DIR "src"
 #define DEST_DIR   "docs"
 #define MAX_LINE_LENGTH 500
 #define MAX_FILES       100
 #define MAX_LINKS       100
+#define FAVICON    "<link rel=icon href=\"data:image/png;base64,\n\
+iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAnXAAAJ1wGxbhe3AAAA\n\
+GXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAchJREFUWIXF1j1oFFEUxfHfrF+J\n\
+hVpFAqksxEoU0TQhRoiNplOs7ATtLERBLPRFMIWCvZ1NCjsbiWAQFUQFwU4iqIUIFgZJIiJmXdci\n\
+6Ca7bzOj+2ZzujnvMuc/9743M6yxssKV406pO4YNCXJnbHTVJZ/XFyoPTqu7lSD4j0YtOoDBYh0I\n\
+XmFPQoAlrbMjHyAYwAf/Mq5i+oG+SoHCsRLCazgvWCiyB45GvEnc7gBgRvCRvCe7qdeCWWxuWjkk\n\
+eNQBwF+tPoKvRiPh8/o9TRGeD1A3FnGnnFHtBkCGIxH/Xqrw1QGCvRhocmu43x0A0fY/E8ymBGh/\n\
+CoLnGEyYNScz4Yob+QBBHz7J26T/o4oRlz1uXMa1s5Rw+GX/Sp64XuNbKQCZN/kAwRccx7vE8VU9\n\
+K9+gqT8yDY27oO56k/tEcHC5Uc6coe5wxH3QbJQDEPRgqMWvdAuAYfQ2eXN2edktgFj7p51QW0uA\n\
+lvaXA3DNduyOrEzHypf/kmWCfdjWEUDVkNbj/Vbwvj3A0q6dwkhH4e0VbT+NEZwsMZwsDyDTX1o4\n\
+P23yMA/gLhZLArjjovl2i43NEgzjLLYmCq7jhS0mnPM90T3T6zemFFVQZGM2gAAAAABJRU5ErkJg\n\
+gg==\">"
 
 // vim:et:ts=4:sts=0:sw=0:fdm=syntax
