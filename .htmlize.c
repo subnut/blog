840cc55568fb7ad854b422bfdc2acb780c0a4a22
Subhaditya Nath <sn03.general@gmail.com>
2022-01-24 21:33:10 +0530

    Remove old cruft from .htmlize.c

--- .htmlize.c
+++ .htmlize.c
@@ -1,12 +1,3 @@
 #include <stdio.h>
 #include "include/htmlize.h"
-
-int
-main(void)
-{
-	int retcode;
-	retcode = htmlize(stdin, stdout);
-	if (retcode == -1)
-		fputs("No lines input\n", stderr);
-	return retcode;
-}
+int main(void) { return htmlize(stdin, stdout); }
