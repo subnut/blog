d0efa8d4368a7e18a2332ee19e144e3b8bd22978
Subhaditya Nath <sn03.general@gmail.com>
2022-04-22 18:18:31 +0530

    Fix header guards

--- include/escape.h
+++ include/escape.h
@@ -1,5 +1,5 @@
-#ifndef FPUT_ESCAPED_H
-#define FPUT_ESCAPED_H
+#ifndef ESCAPE_H
+#define ESCAPE_H
 
 #include <stdio.h>	// for FILE
 
@@ -7,4 +7,4 @@ void	fputc_escaped			(const char,	FILE *);
 void	fputs_escaped			(const char *,	FILE *);
 void	fputs_escaped_allow_charrefs	(const char *,	FILE *);
 
-#endif /* FPUT_ESCAPED_H */
+#endif /* ESCAPE_H */
--- include/perror.h
+++ include/perror.h
@@ -1,9 +1,9 @@
-#ifndef PERROR_R
-#define PERROR_R
+#ifndef PERROR_H
+#define PERROR_H
 
 #include <stdio.h>
 
 #define perror(str) \
 	(fprintf(stderr,"%d:%s:%s(): ",__LINE__,__FILE__,__func__), perror(str))
 
-#endif /* PERROR_R */
+#endif /* PERROR_H */
