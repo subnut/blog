640df2f55b22de282f18c627c6d11da98850ccc4
Subhaditya Nath <sn03.general@gmail.com>
2022-04-26 18:04:05 +0530

    You are a bool, not an int.

--- mod/charref.c
+++ mod/charref.c
@@ -17,18 +17,18 @@ is_named_charref(const char *given_str)
 {
 	/* charrefs MUST start with ampersand */
 	if (given_str[0] != '&')
-		return 0;
+		return false;
 
 	const char *end = strchr(given_str, ';');
 	if (end == NULL)	// ie. ';' not found
-		return 0;
+		return false;
 
 	const char *start = given_str + 1;
 	for (int i = 0; i < (sizeof named_charrefs / sizeof named_charrefs[0]); i++)
 		if (strneql(start, named_charrefs[i], end - start))
-			return 1;
+			return true;
 
-	return 0;
+	return false;
 }
 
 bool
@@ -36,30 +36,30 @@ is_charref(const char *given_str)
 {
 	/* charrefs MUST start with ampersand */
 	if (given_str[0] != '&')
-		return 0;
+		return false;
 
 	const char *end = strchr(given_str, ';');
 	if (end == NULL)	// ie. ';' not found
-		return 0;
+		return false;
 
 	if (given_str[1] == 'x' || given_str[1] == 'X') {
 		/* It's a Hexadecimal numeric character reference */
 		for (const char *p = &given_str[2]; p < end; p++)
 			if (!isxdigit(*p))	// ie. *p isn't xdigit
 				return 0;
-		return 1;
+		return true;
 	}
 	if (isdigit(given_str[1])) {
 		/* It's a Decimal numeric character reference */
 		for (const char *p = &given_str[2]; p < end; p++)
 			if (!isdigit(*p))	// ie. *p isn't digit
-				return 0;
-		return 1;
+				return false;
+		return true;
 	}
 	if (is_named_charref(given_str))
-		return 1;
+		return true;
 
-	return 0;
+	return false;
 }
 
 /* vim:set nowrap fdm=syntax: */
